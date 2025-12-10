209233ac754765317b48f1212e442d785518c744
jeimm08 <20jeimm23@gmail.com>
2025-12-10 15:21:23 -0400

    Add Movies API Server App

--- src/Smdb.Api/Movies/MoviesRouter.cs
+++ src/Smdb.Api/Movies/MoviesRouter.cs
@@ -6,10 +6,15 @@ public class MoviesRouter : HttpRouter
     public MoviesRouter(MoviesController moviesController)
     {
         UseParametrizedRouteMatching();
+        // GET /api/v1/movies
         MapGet("/", moviesController.ReadMovies);
+        // POST /api/v1/movies
         MapPost("/", HttpUtils.ReadRequestBodyAsText, moviesController.CreateMovie);
+        // GET /api/v1/movies/1
         MapGet("/:id", moviesController.ReadMovie);
+        // PUT /api/v1/movies/1
         MapPut("/:id", HttpUtils.ReadRequestBodyAsText, moviesController.UpdateMovie);
+        // DELETE /api/v1/movies/1
         MapDelete("/:id", moviesController.DeleteMovie);
     }
 }
